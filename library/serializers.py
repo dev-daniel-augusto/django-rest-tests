d78c325bccc8276eaea999cf323053cbee9c45ca
Daniel <daniel.augusto.tonetti@gmail.com>
2021-04-30 17:29:36 -0300

    Add serializers.py

---
+++ library/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from .models import (
+                    Book,
+                    Author,
+                    Rating,
+                    Category,
+                    )
+
+
+class BookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = (
+            'title',
+            'pages',
+            'condition',
+            'price',
+            'category',
+        )
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = (
+            'title',
+            'author',
+        )
+
+
+class Ratingerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Rating
+        fields = (
+            'name',
+            'title',
+            'email',
+            'comment',
+            'stars',
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = (
+            'category',
+        )
