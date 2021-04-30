13b041b743b1189f23053ac0d954c9fcda34e157
Daniel <daniel.augusto.tonetti@gmail.com>
2021-04-30 17:15:30 -0300

    Update the admin.py file

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import (
+                    Book,
+                    Author,
+                    Rating,
+                    Category,
+                    )
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'pages', 'condition',  'price', 'category']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title']
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'title', 'email', 'stars']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category']
