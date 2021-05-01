cdc6f118918e658646c2646641393adedf72a43e
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-01 12:13:28 -0300

    Add BookSerializer class validators

--- library/serializers.py
+++ library/serializers.py
@@ -19,6 +19,28 @@ class BookSerializer(serializers.ModelSerializer):
             'category',
         )
 
+    def validate_price(self, value):
+        if value > 0:
+            return value
+        raise serializers.ValidationError(
+            f" You've sent the value {value} to the price field. The price must be higher than 0 ")
+
+    def validate_title(self, string):
+        if string[0] == string.capitalize()[0]:
+            return string
+        raise serializers.ValidationError(
+            f"You've sent the string {string} to the title field. The first letter must be upper case")
+
+    def validate_pages(self, value):
+        if value > 0:
+            if value <= 50560:
+                return value
+        if value < 0:
+            raise serializers.ValidationError(
+                f"You've sent the value {value} to the pages field. The pages number must be higher than 0")
+        raise serializers.ValidationError(
+            f"You've sent the value {value} to the pages field. The max length allowed is 50560 pages ")
+
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
