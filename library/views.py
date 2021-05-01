d60d77fa188aff1bff3d1cca0da6859f132f5690
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-01 11:31:28 -0300

    Create views for library app

--- library/views.py
+++ library/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import (
+                    Book,
+                    Author,
+                    Rating,
+                    Category,
+                    )
+from .serializers import (
+                    BookSerializer,
+                    AuthorSerializer,
+                    RatingSerializer,
+                    CategorySerializer,
+                    )
 
-# Create your views here.
+
+class BookAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class AuthorAPIView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class RatingAPIView(generics.ListCreateAPIView):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+
+class CategoryAPIView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
