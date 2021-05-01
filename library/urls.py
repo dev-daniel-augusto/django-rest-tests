a390dcd5021c31dbf22f5e75c726c75ed87de5be
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-01 14:07:35 -0300

    Update APIs urls

--- library/urls.py
+++ library/urls.py
@@ -1,14 +1,22 @@
 from django.urls import path
 from .views import (
+                    BooksAPIView,
                     BookAPIView,
+                    AuthorsAPIView,
                     AuthorAPIView,
+                    RatingsAPIView,
                     RatingAPIView,
+                    CategoriesAPIView,
                     CategoryAPIView,
                     )
 
 urlpatterns = [
-    path('books/', BookAPIView.as_view(), name='books'),
-    path('authors/', AuthorAPIView.as_view(), name='authors'),
-    path('ratings/', RatingAPIView.as_view(), name='ratings'),
-    path('categories/', CategoryAPIView.as_view(), name='categories')
+    path('books/', BooksAPIView.as_view(), name='books'),
+    path('books/<int:pk>/', BookAPIView.as_view(), name='book'),
+    path('authors/', AuthorsAPIView.as_view(), name='authors'),
+    path('authors/<int:pk>/', AuthorAPIView.as_view(), name='author'),
+    path('ratings/', RatingsAPIView.as_view(), name='ratings'),
+    path('ratings/<int:pk>/', RatingAPIView.as_view(), name='rating'),
+    path('categories/', CategoriesAPIView.as_view(), name='categories'),
+    path('categories/<int:pk>/', CategoryAPIView.as_view(), name="category"),
 ]
