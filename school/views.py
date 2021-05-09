e055f12f9db3c52c9d39953d976163c1d443100a
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-09 09:51:08 -0300

    Add new routes for Student and Parent viewsets

--- school/views.py
+++ school/views.py
@@ -1,19 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import permissions
 from .models import (
                     Parent,
                     Phone,
                     Student,
                     )
 from .serializers import (
-                        ParentSerializer,
-                        PhoneSerializer,
-                        StudentSerializer,
+                          ParentSerializer,
+                          PhoneSerializer,
+                          StudentSerializer,
                          )
 
 
 class ParentVewSet(viewsets.ModelViewSet):
     queryset = Parent.objects.all()
     serializer_class = ParentSerializer
+    permission_classes = (permissions.DjangoModelPermissions, )
+
+    @action(detail=True, methods=['get'])
+    def phones(self, request, pk=None):
+        parent = self.get_object()
+        serializer = PhoneSerializer(parent.telephones.all(), many=True)
+        return Response(serializer.data)
 
 
 class PhoneViewSet(viewsets.ModelViewSet):
@@ -24,3 +34,9 @@ class PhoneViewSet(viewsets.ModelViewSet):
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    @action(detail=True, methods=['get'])
+    def parents(self, request, pk=None):
+        student = self.get_object()
+        serializer = ParentSerializer(student.parent_name.all(), many=True)
+        return Response(serializer.data)
