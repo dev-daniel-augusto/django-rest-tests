9e440e28f9268ceef5b84bdfefb8ee19231e0ebb
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-09 09:49:04 -0300

    Rename a field name from Phone serializer

---
+++ school/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import (
+                    Parent,
+                    Phone,
+                    Student,
+                    )
+
+
+class ParentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Parent
+        fields = [
+            'id',
+            'parent_name',
+            'email'
+        ]
+
+    def normalize_parent_name(self, name):
+        name = name.title()
+        return name
+
+
+class PhoneSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Phone
+        fields = [
+            'id',
+            'parent_name',
+            'mobile_phone',
+            'landline_phone'
+        ]
+
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Student
+        fields = [
+            'id',
+            'parent_name',
+            'student_name',
+            'email',
+        ]
