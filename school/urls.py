afaedb316743cab933bc16c7dd441d24a9086478
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-09 09:54:47 -0300

    Update the school's app urls

--- school/urls.py
+++ school/urls.py
@@ -4,10 +4,24 @@ from .views import (
                     ParentVewSet,
                     PhoneViewSet,
                     StudentViewSet,
+                    ParentsAPIView,
+                    PhonesAPIView,
+                    StudentsAPIView,
+                    ParentAPIView,
+                    PhoneAPIView,
+                    StudentAPIView,
                     )
 
 router = SimpleRouter()
 router.register('parents', ParentVewSet)
-router.register('parents_phones', PhoneViewSet)
+router.register('parents-phones', PhoneViewSet)
 router.register('students', StudentViewSet)
 
+urlpatterns = [
+    path('parents/', ParentsAPIView.as_view(), name='parents'),
+    path('parents-phones/', PhonesAPIView.as_view(), name='phones'),
+    path('students/', StudentsAPIView.as_view(), name='students'),
+    path('parents/<int:pk>/', ParentAPIView.as_view(), name='parent'),
+    path('parents-phones/<int:pk>/', PhoneAPIView.as_view(), name='phone'),
+    path('students/<int:pk>/', StudentAPIView.as_view(), name='student'),
+]
