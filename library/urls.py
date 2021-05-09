5db82341cf02f07320f1901401a0777845724203
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-09 11:00:15 -0300

    Put the library's app urls in order

--- library/urls.py
+++ library/urls.py
@@ -13,10 +13,12 @@ from .views import (
 urlpatterns = [
     path('books/', BooksAPIView.as_view(), name='books'),
     path('books/<int:pk>/', BookAPIView.as_view(), name='book'),
+    path('books/<int:book_pk>/ratings/', RatingsAPIView.as_view(), name='book_ratings'),
+    path('books/<int:book_pk/ratings/<int:rating_pk/', RatingAPIView.as_view(), name='book_rating'),
     path('authors/', AuthorsAPIView.as_view(), name='authors'),
     path('authors/<int:pk>/', AuthorAPIView.as_view(), name='author'),
     path('ratings/', RatingsAPIView.as_view(), name='ratings'),
-    path('ratings/<int:pk>/', RatingAPIView.as_view(), name='rating'),
+    path('ratings/<int:rating_pk>/', RatingAPIView.as_view(), name='rating'),
     path('categories/', CategoriesAPIView.as_view(), name='categories'),
     path('categories/<int:pk>/', CategoryAPIView.as_view(), name="category"),
 ]
