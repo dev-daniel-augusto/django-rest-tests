778971652146797064ba45ce97379fc9686dd4fa
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-09 11:00:46 -0300

    Update the library views

--- library/views.py
+++ library/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
 from .models import (
                     Book,
                     Author,
@@ -37,10 +38,22 @@ class RatingsAPIView(generics.ListCreateAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('book_pk'):
+            return self.queryset.filter(id=self.kwargs.get('book_pk'))
+        return self.queryset.all()
+
 
 class RatingAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+
+    def get_object(self):
+        if self.kwargs.get('book_pk'):
+            return get_object_or_404(self.get_queryset(),
+                                     self.queryset.filter('book_pk'),
+                                     pk=self.kwargs.get('ratings_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('ratings_pk'))
 
 
 class CategoriesAPIView(generics.ListCreateAPIView):
