bc33fcf2d4952bc6e4bbbd7825b2f2fda21f3930
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-10 12:45:43 -0300

    Update school's app views

--- school/views.py
+++ school/views.py
@@ -2,15 +2,20 @@ from rest_framework import viewsets, generics
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import permissions
+from rest_framework.generics import get_object_or_404
 from .models import (
                     Parent,
                     Phone,
                     Student,
+                    Subject,
+                    Teacher,
                     )
 from .serializers import (
                           ParentSerializer,
                           PhoneSerializer,
                           StudentSerializer,
+                          SubjectSerializer,
+                          TeacherSerializer,
                          )
 
 
@@ -66,7 +71,44 @@ class StudentsAPIView(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('parent_pk'):
+            return self.queryset.filter(id=self.kwargs.get('parent_pk'))
+        return self.queryset.all()
+
 
 class StudentAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    def get_object(self):
+        if self.kwargs.get('parent_pk'):
+            return get_object_or_404(self.get_queryset(),
+                                     id=self.kwargs.get('parent_pk'),
+                                     pk=self.kwargs.get('student_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('student_pk'))
+
+
+class SubjectsAPIView(generics.ListCreateAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('subject_pk'):
+            return self.queryset.filter(id=self.kwargs.get('subject_pk'))
+        return self.queryset.all()
+
+
+class SubjectAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class TeachersAPIView(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+class TeacherAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
