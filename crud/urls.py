48d3ec37fe151a7f7ff3bdc2cd3d77294c63cf07
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-12 16:23:31 -0300

    Update crud's app urls file

--- crud/urls.py
+++ crud/urls.py
@@ -1,14 +1,22 @@
 from django.urls import path
 from .views import (
-                    IndexView,
+                    ParentView,
                     CreateParentView,
                     UpdateParentView,
                     DeleteParentView,
+                    PhoneView,
+                    CreatePhoneView,
+                    UpdatePhoneView,
+                    DeletePhoneView,
                     )
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('parent/add/', CreateParentView.as_view(), name='create_parent'),
-    path('parent/update/<int:pk>', UpdateParentView.as_view(), name='update_parent'),
-    path('parent/delete/<int:pk>', DeleteParentView.as_view(), name='delete_parent')
+    path('parents/', ParentView.as_view(), name='parents'),
+    path('parents/add/', CreateParentView.as_view(), name='create_parent'),
+    path('parents/update/<int:pk>', UpdateParentView.as_view(), name='update_parent'),
+    path('parents/delete/<int:pk>', DeleteParentView.as_view(), name='delete_parent'),
+    path('phones/', PhoneView.as_view(), name='phones'),
+    path('phones/add/', CreatePhoneView.as_view(), name='create_phone'),
+    path('phones/update/<int:pk>/', UpdatePhoneView.as_view(), name='update_phone'),
+    path('phones/update/int:<pk>/', DeletePhoneView.as_view(), name='delete_phone'),
 ]
