3c1a1fdf5eb3da1432ca0e940d52ad0223134900
Daniel <daniel.augusto.tonetti@gmail.com>
2021-05-13 10:31:38 -0300

    Update crud's app urls file

--- crud/urls.py
+++ crud/urls.py
@@ -8,6 +8,10 @@ from .views import (
                     CreatePhoneView,
                     UpdatePhoneView,
                     DeletePhoneView,
+                    StudentView,
+                    CreateStudentView,
+                    UpdateStudentView,
+                    DeleteStudentView,
                     )
 
 urlpatterns = [
@@ -19,4 +23,9 @@ urlpatterns = [
     path('phones/add/', CreatePhoneView.as_view(), name='create_phone'),
     path('phones/update/<int:pk>/', UpdatePhoneView.as_view(), name='update_phone'),
     path('phones/update/int:<pk>/', DeletePhoneView.as_view(), name='delete_phone'),
+    path('students/', StudentView.as_view(), name='students'),
+    path('students/add/', CreateStudentView.as_view(), name='create_student'),
+    path('students/update/<int:pk>/', UpdateStudentView.as_view(), name='update_student'),
+    path('students/delete/<int:pk>/', DeleteStudentView.as_view(), name='delete_student'),
 ]
+
