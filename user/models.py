61f224d31eb264d6083d60e98526e428d3fae455
Daniel <daniel.augusto.tonetti@gmail.com>
2021-07-03 13:43:55 -0300

    Create a custom user manager

--- user/models.py
+++ user/models.py
@@ -2,24 +2,55 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, first_name, last_name, password=None):
+        if not email:
+            raise ValueError('You must have an email')
+        if not username:
+            raise ValueError('You must have an username')
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, first_name, last_name, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self.__db)
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     GENDER_CHOICES = (
         ('Female', 'Female'),
         ('Male', 'Male'),
-        ('None', 'None')
+        ('Non-Binary', 'Non-Binary')
     )
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=True)
-    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
-    objects = None
+    objects = CustomUserManager
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = 'username'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.email
