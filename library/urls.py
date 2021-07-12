5a6634dd358be2a7acceb35b455fbcbe5cdc9424
Daniel <daniel.augusto.tonetti@gmail.com>
2021-07-12 20:44:17 -0300

    Update the library's app urls

--- library/urls.py
+++ library/urls.py
@@ -1,24 +1,17 @@
 from django.urls import path
+
 from .views import (
-                    BooksAPIView,
-                    BookAPIView,
-                    AuthorsAPIView,
-                    AuthorAPIView,
-                    RatingsAPIView,
-                    RatingAPIView,
-                    CategoriesAPIView,
-                    CategoryAPIView,
-                    )
+    AuthorsAPIView, AuthorAPIView,
+    LanguagesAPIView, LanguageAPIView,
+    BooksAPIView, BookAPIView,
+)
+
 
 urlpatterns = [
-    path('books/', BooksAPIView.as_view(), name='books'),
-    path('books/<int:pk>/', BookAPIView.as_view(), name='book'),
-    path('books/<int:book_pk>/ratings/', RatingsAPIView.as_view(), name='book_ratings'),
-    path('books/<int:book_pk/ratings/<int:rating_pk/', RatingAPIView.as_view(), name='book_rating'),
-    path('authors/', AuthorsAPIView.as_view(), name='authors'),
-    path('authors/<int:pk>/', AuthorAPIView.as_view(), name='author'),
-    path('ratings/', RatingsAPIView.as_view(), name='ratings'),
-    path('ratings/<int:rating_pk>/', RatingAPIView.as_view(), name='rating'),
-    path('categories/', CategoriesAPIView.as_view(), name='categories'),
-    path('categories/<int:pk>/', CategoryAPIView.as_view(), name="category"),
+    path('books/', BooksAPIView.as_view(), name='books_api'),
+    path('books/<int:pk>/', BookAPIView.as_view(), name='book_api'),
+    path('authors/', AuthorsAPIView.as_view(), name='authors_api'),
+    path('authors/<int:pk>/', AuthorAPIView.as_view(), name='author_api'),
+    path('languages/', LanguagesAPIView.as_view(), name='languages_api'),
+    path('languages/<int:pk>/', LanguageAPIView.as_view(), name='language_api'),
 ]
