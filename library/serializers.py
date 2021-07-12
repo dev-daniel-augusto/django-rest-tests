1ed96502ef5a460dc48a5d8226e2db989bf7e6f4
Daniel <daniel.augusto.tonetti@gmail.com>
2021-07-12 15:35:55 -0300

    Update serializers file to reflect the models update

--- library/serializers.py
+++ library/serializers.py
@@ -1,10 +1,41 @@
 from rest_framework import serializers
+
 from .models import (
-                    Book,
-                    Author,
-                    Rating,
-                    Category,
-                    )
+    Author,
+    Language,
+    Category,
+    Publisher,
+    Book,
+    Rating,
+)
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Author
+        fields = ('author_name', 'created', 'modified')
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Language
+        fields = ('language_name', 'created', 'modified')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('category_name', 'created', 'modified')
+
+
+class PublisherSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Publisher
+        fields = ('publisher_name', 'created', 'modified')
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -12,67 +43,28 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = (
-            'id',
-            'title',
-            'pages',
-            'condition',
-            'price',
-            'category',
-        )
-
-    def validate_price(self, value):
-        if value > 0:
-            return value
-        raise serializers.ValidationError(
-            f" You've sent the value {value} to the price field. The price must be higher than 0")
-
-    def validate_title(self, string):
-        if string[0] == string.capitalize()[0]:
-            return string
-        raise serializers.ValidationError(
-            f"You've sent the string {string} to the title field. The first letter must be upper case")
-
-    def validate_pages(self, value):
-        if value > 0:
-            if value <= 50560:
-                return value
-        if value < 0:
-            raise serializers.ValidationError(
-                f"You've sent the value {value} to the pages field. The pages number must be higher than 0")
-        raise serializers.ValidationError(
-            f"You've sent the value {value} to the pages field. The max length allowed is 50560 pages")
-
-
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = (
-            'id',
-            'title',
+            'book_name',
             'author',
+            'category',
+            'number_of_pages',
+            'language',
+            'publisher',
+            'condition',
+            'book_description',
         )
 
 
-class RatingSerializer(serializers.ModelSerializer):
+class RatingSerializer:
+
     class Meta:
+        model = Rating
         extra_kwargs = {
             'email': {'write_only': True}
         }
-        model = Rating
         fields = (
-            'id'
             'title',
-            'name',
-            'email',
-            'comment',
+            'customer',
+            'review',
             'stars',
-        )
-
-
-class CategorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Category
-        fields = (
-            'category',
+            'email',
         )
