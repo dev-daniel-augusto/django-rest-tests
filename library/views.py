c521b794fea6376d12679589c925bd7042982fc2
Daniel <daniel.augusto.tonetti@gmail.com>
2021-07-12 20:42:34 -0300

    Create an API views for both Author and Language models

--- library/views.py
+++ library/views.py
@@ -1,22 +1,14 @@
 from rest_framework import generics
-from django.shortcuts import get_object_or_404
-from .models import (
-                    Book,
-                    Author,
-                    Rating,
-                    Category,
-                    )
-from .serializers import (
-                    BookSerializer,
-                    AuthorSerializer,
-                    RatingSerializer,
-                    CategorySerializer,
-                    )
+
+from .models import Author, Language, Book
+from .filters import AuthorFilter, LanguageFilter, BookFilter
+from .serializers import AuthorSerializer, LanguageSerializer, BookSerializer
 
 
 class BooksAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filterset_class = BookFilter
 
 
 class BookAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -27,6 +19,7 @@ class BookAPIView(generics.RetrieveUpdateDestroyAPIView):
 class AuthorsAPIView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    filterset_class = AuthorFilter
 
 
 class AuthorAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -34,33 +27,12 @@ class AuthorAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AuthorSerializer
 
 
-class RatingsAPIView(generics.ListCreateAPIView):
-    queryset = Rating.objects.all()
-    serializer_class = RatingSerializer
-
-    def get_queryset(self):
-        if self.kwargs.get('book_pk'):
-            return self.queryset.filter(id=self.kwargs.get('book_pk'))
-        return self.queryset.all()
+class LanguagesAPIView(generics.ListCreateAPIView):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+    filterset_class = LanguageFilter
 
 
-class RatingAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Rating.objects.all()
-    serializer_class = RatingSerializer
-
-    def get_object(self):
-        if self.kwargs.get('book_pk'):
-            return get_object_or_404(self.get_queryset(),
-                                     self.queryset.filter('book_pk'),
-                                     pk=self.kwargs.get('ratings_pk'))
-        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('ratings_pk'))
-
-
-class CategoriesAPIView(generics.ListCreateAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-
-class CategoryAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+class LanguageAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
