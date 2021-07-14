f266c361031f57c898cf5d90a0bcf1c1a2e0d1f1
Daniel <daniel.augusto.tonetti@gmail.com>
2021-07-14 13:30:17 -0300

    Create Category, Publisher and Rating API views

--- library/views.py
+++ library/views.py
@@ -1,8 +1,29 @@
 from rest_framework import generics
 
-from .models import Author, Language, Book
-from .filters import AuthorFilter, LanguageFilter, BookFilter
-from .serializers import AuthorSerializer, LanguageSerializer, BookSerializer
+from .models import (
+    Author,
+    Language,
+    Category,
+    Publisher,
+    Book,
+    Rating,
+)
+from .filters import (
+    AuthorFilter,
+    LanguageFilter,
+    CategoryFilter,
+    PublisherFilter,
+    BookFilter,
+    RatingFilter,
+)
+from .serializers import (
+    AuthorSerializer,
+    LanguageSerializer,
+    CategorySerializer,
+    PublisherSerializer,
+    BookSerializer,
+    RatingSerializer,
+)
 
 
 class BooksAPIView(generics.ListCreateAPIView):
@@ -36,3 +57,36 @@ class LanguagesAPIView(generics.ListCreateAPIView):
 class LanguageAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
+
+class CategoriesAPIView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filterset_class = CategoryFilter
+
+
+class CategoryAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class PublishersAPIView(generics.ListCreateAPIView):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+    filterset_class = PublisherFilter
+
+
+class PublisherAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+
+
+class RatingsAPIView(generics.ListCreateAPIView):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    filterset_class = RatingFilter
+
+
+class RatingAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Rating.objects.all()
+    serializer_class = PublisherSerializer
