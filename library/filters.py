b818d147fac5a696cb4489d0c205be414a623313
Daniel <daniel.augusto.tonetti@gmail.com>
2021-07-14 13:13:05 -0300

    Create Category, Publisher and Rating filter classes

--- library/filters.py
+++ library/filters.py
@@ -1,6 +1,13 @@
 import django_filters
 
-from .models import Author, Language, Book
+from .models import (
+    Author,
+    Language,
+    Category,
+    Publisher,
+    Book,
+    Rating,
+)
 
 
 class AuthorFilter(django_filters.FilterSet):
@@ -27,6 +34,30 @@ class LanguageFilter(django_filters.FilterSet):
         fields = ('language_name',)
 
 
+class CategoryFilter(django_filters.FilterSet):
+    category_name = django_filters.CharFilter(lookup_expr='icontains', label='Category')
+    category_name__istartswith = django_filters.CharFilter(field_name='category_name', lookup_expr='istartswith',
+                                                           label='Category (Starts with):')
+    category_name__iendswith = django_filters.CharFilter(field_name='category_name', lookup_expr='iendswith',
+                                                         label='Category (Ends with):')
+
+    class Meta:
+        model = Category
+        fields = ('category_name',)
+
+
+class PublisherFilter(django_filters.FilterSet):
+    publisher_name = django_filters.CharFilter(lookup_expr='icontains', label='Publisher')
+    publisher_name__istartswith = django_filters.CharFilter(field_name='publisher_name', lookup_expr='istartswith',
+                                                            label='Publisher (Starts with):')
+    publisher_name__iendswith = django_filters.CharFilter(field_name='publisher_name', lookup_expr='iendswith',
+                                                          label='Publisher (Ends with):')
+
+    class Meta:
+        model = Publisher
+        fields = ('publisher_name',)
+
+
 class BookFilter(django_filters.FilterSet):
     book_name = django_filters.CharFilter(lookup_expr='icontains', label='Book Name:')
     book_name__istartswith = django_filters.CharFilter(field_name='book_name', lookup_expr='istartswith',
@@ -49,3 +80,25 @@ class BookFilter(django_filters.FilterSet):
     class Meta:
         model = Book
         fields = ('book_name',)
+
+
+class RatingFilter(django_filters.FilterSet):
+    customer_name = django_filters.CharFilter(lookup_expr='icontains', label='Costumer')
+    customer_name__istartswith = django_filters.CharFilter(field_name='customer_name', lookup_expr='istartswith',
+                                                           label='Customer (Starts with):')
+    customer_name__iendswith = django_filters.CharFilter(field_name='customer_name', lookup_expr='iendswith',
+                                                         label='Customer (Ends with):')
+
+    title__book_name = django_filters.CharFilter(lookup_expr='icontains', label='Book Name')
+    title__book_name__istartswith = django_filters.CharFilter(field_name='title__book_name', lookup_expr='istartswith',
+                                                              label='Book Name (Starts with):')
+    title__book_name__iendswith = django_filters.CharFilter(field_name='title__book_name', lookup_expr='iendswith',
+                                                            label='Book Name (Ends with):')
+
+    review = django_filters.CharFilter(lookup_expr='icontains', label='Review (Search by keyword):')
+
+    stars = django_filters.ChoiceFilter(choices=Rating.Star.choices)
+
+    class Meta:
+        model = Rating
+        fields = ('customer_name',)
